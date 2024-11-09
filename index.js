3aef2f842d29f7ab96e622f78fb4db7ebccaa7ba
Mohan-8 <smohanakrishnan82@gmail.com>
2024-11-09 21:39:19 +0530

    updated backend

--- index.js
+++ index.js
@@ -5,18 +5,14 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 
-// Initialize the Telegram bot with your Telegram API token
 const token = process.env.TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 console.log("Telegram Bot is running");
-
-// MongoDB connection
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected successfully"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-// Define the User model
 const UserSchema = new mongoose.Schema({
   telegramId: { type: String, required: true, unique: true },
   firstName: { type: String, required: true },
@@ -43,21 +39,19 @@ const UserSchema = new mongoose.Schema({
 });
 const User = mongoose.model("User", UserSchema);
 
-// Create an Express app
 const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: "*", // Restrict this in production
+  origin: "*",
   methods: ["GET", "POST"],
   credentials: true,
 };
 
 app.use(cors(corsOptions));
-// Handle the /start command with or without a referral ID
 bot.onText(/\/start(?:\s+(.+))?/, async (msg, match) => {
   const chatId = msg.chat.id;
-  const referrerId = match[1]; // Extract the referrer ID from the referral link
+  const referrerId = match[1];
   const { id, first_name: firstName, last_name: lastName = "" } = msg.from;
 
   let user = await User.findOne({ telegramId: id });
@@ -66,15 +60,13 @@ bot.onText(/\/start(?:\s+(.+))?/, async (msg, match) => {
     user = new User({ telegramId: id, firstName, lastName });
 
     if (referrerId) {
-      user.referredBy = referrerId; // Store the referrer
+      user.referredBy = referrerId;
       const referrer = await User.findOne({ telegramId: referrerId });
       if (referrer) {
-        referrer.referralCount += 1; // Increment the referrer’s count
-        const pointsAwarded = calculateReferralPoints(referrer.referralCount); // Calculate points based on referrals
-        referrer.rewards += pointsAwarded; // Add points to rewards
-        await referrer.save(); // Save the referrer’s new rewards
-
-        // Notify the referrer about their new points
+        referrer.referralCount += 1;
+        const pointsAwarded = calculateReferralPoints(referrer.referralCount);
+        referrer.rewards += pointsAwarded;
+        await referrer.save();
         bot.sendMessage(
           referrerId,
           `You referred ${user.firstName} ${user.lastName} and earned ${pointsAwarded} points!`
@@ -100,30 +92,10 @@ bot.onText(/\/start(?:\s+(.+))?/, async (msg, match) => {
     },
   };
 
-  // Send welcome message
   const welcomeMessage = `Welcome, ${user.firstName}! Click the button below to check your stats.`;
   bot.sendMessage(chatId, welcomeMessage, inlineKeyboard);
 });
 
-// Generate JWT token for authentication
-const generateToken = (userId) => {
-  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "1h" });
-};
-const authenticateJWT = (req, res, next) => {
-  const token = req.headers["authorization"]?.split(" ")[1];
-  if (token) {
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-      if (err) {
-        return res.sendStatus(403);
-      }
-      req.user = user;
-      next();
-    });
-  } else {
-    res.sendStatus(401);
-  }
-};
-// Fetch user data based on userId (endpoint for the frontend to retrieve user info)
 app.get("/api/user/:userId", async (req, res) => {
   const { userId } = req.params;
 
@@ -134,22 +106,21 @@ app.get("/api/user/:userId", async (req, res) => {
     }
 
     const now = new Date();
-    const claimInterval = 8 * 60 * 60 * 1000; // 1 minute for testing, change to 8 hours (8 * 60 * 60 * 1000) for production
+    const claimInterval = 8 * 60 * 60 * 1000;
     let timeRemaining = 0;
     let canClaim = false;
     let streakCount = user.streakCount;
     let lastlogin = user.lastLoginAt;
-    // If the user has never claimed, they can claim immediately
     if (!user.lastClaimedAt) {
       canClaim = true;
-      timeRemaining = claimInterval / 1000; // Convert milliseconds to seconds
+      timeRemaining = claimInterval / 1000;
     } else {
-      const elapsedTime = now - user.lastClaimedAt; // Calculate elapsed time in milliseconds
+      const elapsedTime = now - user.lastClaimedAt;
 
       if (elapsedTime >= claimInterval) {
-        canClaim = true; // User can claim again
+        canClaim = true;
       } else {
-        timeRemaining = (claimInterval - elapsedTime) / 1000; // Calculate remaining time in seconds
+        timeRemaining = (claimInterval - elapsedTime) / 1000;
       }
     }
 
@@ -161,7 +132,7 @@ app.get("/api/user/:userId", async (req, res) => {
       canClaim,
       timeRemaining,
       streakCount,
-      lastlogin, // Send remaining time
+      lastlogin,
     });
   } catch (error) {
     console.error("Error fetching user data:", error);
@@ -171,8 +142,6 @@ app.get("/api/user/:userId", async (req, res) => {
   }
 });
 
-// Claim rewards endpoint
-// Claim points endpoint
 app.post("/api/user/:userId/claim", async (req, res) => {
   const { userId } = req.params;
   const { points } = req.body;
@@ -182,7 +151,7 @@ app.post("/api/user/:userId/claim", async (req, res) => {
       { telegramId: userId },
       {
         $inc: { rewards: points },
-        $set: { hasClaimed: true, lastClaimedAt: new Date() }, // Store current timestamp
+        $set: { hasClaimed: true, lastClaimedAt: new Date() },
       }
     );
     res.status(200).json({ message: "Points claimed successfully." });
@@ -216,7 +185,6 @@ app.get("/api/user/referrals/:userId", async (req, res) => {
 
     const referredCount = referralDetails.length;
 
-    // Get the referrer user
     const referrer = await User.findOne({ telegramId: userId });
 
     res.json({
@@ -233,10 +201,9 @@ app.get("/api/user/referrals/:userId", async (req, res) => {
 });
 
 app.post("/api/user/:userId/claimReferralReward", async (req, res) => {
-  const userId = req.params.userId; // This is your telegramId
+  const userId = req.params.userId;
   const { index } = req.body;
 
-  // Validate the index
   if (typeof index !== "number" || index < 0 || index > 4) {
     return res.status(400).json({ message: "Invalid index" });
   }
@@ -247,14 +214,12 @@ app.post("/api/user/:userId/claimReferralReward", async (req, res) => {
       return res.status(404).json({ message: "User not found" });
     }
 
-    // Check if the reward for this milestone has already been claimed
     if (user.refRewardClaimed[index]) {
       return res.status(400).json({ message: "Reward already claimed" });
     }
 
-    // Mark the reward as claimed
     user.refRewardClaimed[index] = true;
-    const rewardsArray = [250, 1000, 2500, 6000, 21550]; // Updated rewards
+    const rewardsArray = [250, 1000, 2500, 6000, 21550];
     user.rewards += rewardsArray[index];
 
     await user.save();
@@ -277,7 +242,6 @@ const getTodayGMT = () => {
   );
 };
 
-// Handle login and streak update
 const streakRewards = [
   100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400,
   1500, 1600, 1700, 1800, 1900, 2000, 2100,
@@ -328,7 +292,6 @@ const handleLogin = async (userId) => {
   };
 };
 
-// Endpoint to handle user login
 app.post("/api/user/:userId/login", async (req, res) => {
   const { userId } = req.params;
   try {
@@ -347,7 +310,6 @@ app.post("/api/user/:userId/login", async (req, res) => {
   }
 });
 
-// Endpoint for fetching streak data
 app.get("/api/user/:userId/streak", async (req, res) => {
   try {
     const user = await User.findOne({ telegramId: req.params.userId });
@@ -379,7 +341,6 @@ app.post("/api/user/:userId/airdropAction", async (req, res) => {
       return res.status(404).json({ message: "User not found" });
     }
 
-    // Check if the action is valid
     if (!points[action]) {
       return res.status(400).json({ message: "Invalid action" });
     }
@@ -394,7 +355,7 @@ app.post("/api/user/:userId/airdropAction", async (req, res) => {
     user.rewards += points[action];
     user.airdropClaimed[actionIndex] = true;
 
-    await user.save(); // Save changes to the database
+    await user.save();
 
     res
       .status(200)
@@ -475,7 +436,6 @@ app.get("/api/user/:userId/solanaInfo", async (req, res) => {
   }
 });
 
-// Start the Express server
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
